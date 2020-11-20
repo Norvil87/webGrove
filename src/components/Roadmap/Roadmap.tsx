a708e76d45120ad0ddff428f8a9a51058f8b6f3a
Степанов Дмитрий Андреевич <stepanov@INFORMCONTACT.RU>
2020-11-20 17:14:36 +0300

    new links in roadmap

--- src/components/Roadmap/Roadmap.tsx
+++ src/components/Roadmap/Roadmap.tsx
@@ -85,6 +85,49 @@ const Roadmap: React.FC = () => {
         >
           <span>Как работают браузеры</span>
         </a>
+
+        <a
+          href="https://developer.mozilla.org/ru/docs/%D0%A1%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D1%8C/HTML"
+          target="_blank"
+          className="roadmap__link  roadmap__article-link htmlbase-link"
+        >
+          <span>Изучение основ</span>
+        </a>
+        <a
+          href="https://developer.mozilla.org/ru/docs/%D0%A1%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D1%8C/Semantics"
+          target="_blank"
+          className="roadmap__link  roadmap__article-link htmlsemantics-link"
+        >
+          <span>Семантический HTML</span>
+        </a>
+        <a
+          href="https://developer.mozilla.org/ru/docs/Learn/HTML/Forms"
+          target="_blank"
+          className="roadmap__link  roadmap__article-link htmlforms-link"
+        >
+          <span>Формы и валидация</span>
+        </a>
+        <a
+          href="https://developer.mozilla.org/ru/docs/HTML/HTML5/%D0%92%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B2_HTML5"
+          target="_blank"
+          className="roadmap__link  roadmap__article-link html5-link"
+        >
+          <span>Стандарт HTML5</span>
+        </a>
+        <a
+          href="https://developer.mozilla.org/ru/docs/%D0%A1%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D1%8C/SEO"
+          target="_blank"
+          className="roadmap__link  roadmap__article-link htmlseo-link"
+        >
+          <span>Основы SEO</span>
+        </a>
+        <a
+          href="https://developer.mozilla.org/ru/docs/Learn/%D0%94%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%BD%D0%BE%D1%81%D1%82%D1%8C"
+          target="_blank"
+          className="roadmap__link  roadmap__article-link htmlaccessability-link"
+        >
+          <span>Доступность</span>
+        </a>
       </div>
     </section>
   );
